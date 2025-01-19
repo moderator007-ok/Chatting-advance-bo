cb86c5659608e551445aba3312b60f9bac9be4e1
moderator007-ok <meizharryputtar@gmail.com>
2025-01-20 02:05:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ async def init():
             pass
 
     @app.on_message(filters.private)
-    async def incoming_private(_, message):
+    async def incoming_private(_, message: Message):
         # Ignore edited messages
         if message.edit_date:
             return
@@ -245,60 +245,48 @@ async def init():
         user_id = message.from_user.id
         if await mongo.is_banned_user(user_id):
             return
-        if user_id in SUDO_USERS:
-            if message.reply_to_message:
-                if (
-                    message.text == "/unblock"
-                    or message.text == "/block"
-                    or message.text == "/broadcast"
-                ):
-                    return
-                if not message.reply_to_message.forward_sender_name:
-                    return await message.reply_text(
-                        "Please reply to forwarded messages only."
-                    )
-                replied_id = message.reply_to_message_id
-                try:
-                    replied_user_id = save[replied_id]
-                except Exception as e:
-                    print(e)
-                    return await message.reply_text(
-                        "Failed to fetch user. You might've restarted bot or some error happened. Please check logs"
-                    )
-                try:
-                    return await app.copy_message(
-                        replied_user_id,
-                        message.chat.id,
-                        message.message_id,
-                    )
-                except Exception as e:
-                    print(e)
-                    return await message.reply_text(
-                        "Failed to send the message, User might have blocked the bot or something wrong happened. Please check logs"
-                    )
+        if message.reply_to_message:
+            if (
+                message.text == "/unblock"
+                or message.text == "/block"
+                or message.text == "/broadcast"
+            ):
+                return
+            if not message.reply_to_message.forward_sender_name:
+                return await message.reply_text(
+                    "Please reply to forwarded messages only."
+                )
+            replied_id = message.reply_to_message_id
+            try:
+                replied_user_id = save[replied_id]
+            except Exception as e:
+                print(e)
+                return await message.reply_text(
+                    "Failed to fetch user. You might've restarted bot or some error happened. Please check logs"
+                )
+            try:
+                return await app.copy_message(
+                    replied_user_id,
+                    message.chat.id,
+                    message.message_id,
+                )
+            except Exception as e:
+                print(e)
+                return await message.reply_text(
+                    "Failed to send the message, User might have blocked the bot or something wrong happened. Please check logs"
+                )
         else:
-            if await mongo.is_group():
+            for user in SUDO_USERS:
                 try:
                     forwarded = await app.forward_messages(
-                        config.LOG_GROUP_ID,
-                        message.chat.id,
-                        message.message_id,
+                        user, message.chat.id, message.message_id
                     )
                     save[forwarded.message_id] = user_id
                 except:
                     pass
-            else:
-                for user in SUDO_USERS:
-                    try:
-                        forwarded = await app.forward_messages(
-                            user, message.chat.id, message.message_id
-                        )
-                        save[forwarded.message_id] = user_id
-                    except:
-                        pass
 
     @app.on_message(filters.group)
-    async def incoming_groups(_, message):
+    async def incoming_groups(_, message: Message):
         # Ignore edited messages
         if message.edit_date:
             return
